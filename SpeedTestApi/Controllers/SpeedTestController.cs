0dca3a2de0a1eae098202f3da350be13121af346
cxtrond <tsm@computas.com>
2020-02-12 15:47:45 +0100

    Added analytics

--- SpeedTestApi/Controllers/SpeedTestController.cs
+++ SpeedTestApi/Controllers/SpeedTestController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SpeedTestApi.Models;
+using SpeedTestApi.Services;
+using System.Threading.Tasks;
 
 
 namespace SpeedTestApi.Controllers
@@ -11,10 +13,12 @@ namespace SpeedTestApi.Controllers
     public class SpeedTestController : ControllerBase
     {
         private readonly ILogger _logger;
+        private readonly ISpeedTestEvents _eventHub;
 
-        public SpeedTestController(ILogger<SpeedTestController> logger)
+        public SpeedTestController(ILogger<SpeedTestController> logger, ISpeedTestEvents eventHub)
         {
             _logger = logger;
+            _eventHub = eventHub;
         }
 
         // GET speedtest/ping
@@ -26,8 +30,10 @@ namespace SpeedTestApi.Controllers
         }
         // POST speedtest/
         [HttpPost]
-        public string UploadSpeedTest([FromBody] TestResult speedTest)
+        public async Task<string> UploadSpeedTest([FromBody] TestResult speedTest)
         {
+            await _eventHub.PublishSpeedTest(speedTest);
+
             var response = $"Got a TestResult from { speedTest.User } with download { speedTest.Data.Speeds.Download } Mbps.";
             _logger.LogInformation(response);
 
